10e595e8e04165a773cbd56b175bdaae200ad5b0
transonhy96 <2307+transonhy96@users.noreply.github.com>
2017-12-19 18:02:20 +0700

    lan cuoi that su

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports ={
-    database: 'mongodb://<transonhy96>:<nabongsun2307>@ds129946.mlab.com:29946/hopamchuan',
+    database: 'mongodb://transonhy96:nabongsun2307@ds129946.mlab.com:29946/hopamchuan',
     secret:'easytoguess'
 }
